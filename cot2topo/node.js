b57bfebdcf33ae37d9708337984ba91755700b9f
clptak <99409651+clptak@users.noreply.github.com>
2023-08-01 18:01:37 -0700

    Update node.js

--- cot2topo/node.js
+++ cot2topo/node.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function(RED) {
-    const subflowFile = path.join(__dirname,"node-red-cot-pli-to-topo.json");
+    const subflowFile = path.join(__dirname,"cot2xtopo.json");
     const subflowContents = fs.readFileSync(subflowFile);
     const subflowJSON = JSON.parse(subflowContents);
     RED.nodes.registerSubflow(subflowJSON);
