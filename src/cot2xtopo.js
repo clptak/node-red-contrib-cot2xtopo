1f6d6b9871e98110968ce246fb9441cd8630cff8
clptak <99409651+clptak@users.noreply.github.com>
2023-10-16 14:39:01 -0700

    Update cot2xtopo.js

--- src/cot2xtopo.js
+++ src/cot2xtopo.js
@@ -2,12 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function(RED) {
-    const subflowFileA = path.join(__dirname,"cot2xtopo.json");
-    const subflowContentsA = fs.readFileSync(subflowFileA);
-    const subflowJSONA = JSON.parse(subflowContentsA);
-    RED.nodes.registerSubflow(subflowJSONA);
-    const subflowFileB = path.join(__dirname,"xtopo2cot.json");
-    const subflowContentsB = fs.readFileSync(subflowFileB);
-    const subflowJSONB = JSON.parse(sublflowContentsB);
-    RED.nodes.registerSubflow(subflowJSONB);
+    const subflowFile = path.join(__dirname,"cot2xtopo.json");
+    const subflowContents = fs.readFileSync(subflowFile);
+    const subflowJSON = JSON.parse(subflowContents);
+    RED.nodes.registerSubflow(subflowJSON);
 }
